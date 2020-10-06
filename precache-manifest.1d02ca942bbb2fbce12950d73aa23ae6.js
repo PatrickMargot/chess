c428d550b159575ec214b98b418b438aac11e9d1
Patrick <margotpatrick02@gmail.com>
2020-10-06 10:46:56 +0200

    Updates

---
+++ precache-manifest.1d02ca942bbb2fbce12950d73aa23ae6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eae9e381be15015be965f4985b6546b",
+    "url": "/chess/index.html"
+  },
+  {
+    "revision": "4432b7f470da1aeea032",
+    "url": "/chess/static/css/main.1b6ada8a.chunk.css"
+  },
+  {
+    "revision": "0d45b60709b0c22fb031",
+    "url": "/chess/static/js/2.46fee98a.chunk.js"
+  },
+  {
+    "revision": "bd919d0b1f5070ed8ecb5e9b4284b891",
+    "url": "/chess/static/js/2.46fee98a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4432b7f470da1aeea032",
+    "url": "/chess/static/js/main.9b42f1aa.chunk.js"
+  },
+  {
+    "revision": "fe54e1bce9cc9d1a3729",
+    "url": "/chess/static/js/runtime-main.bff3f106.js"
+  }
+]);
