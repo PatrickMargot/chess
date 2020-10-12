94b316cb64f17bad32dba01298d67847e13880b5
Patrick <margotpatrick02@gmail.com>
2020-10-12 19:01:21 +0200

    Updates

--- precache-manifest.f9b7e9c93bee22028d46e7215335be52.js
+++ precache-manifest.f9b7e9c93bee22028d46e7215335be52.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "65774207afe96769f9f8e9fb3f1e41cc",
+    "revision": "a2bba6199b0b431ec032e42f63a2c346",
     "url": "/chess/index.html"
   },
   {
-    "revision": "777c5e9feb0796b4e054",
-    "url": "/chess/static/css/main.1b6ada8a.chunk.css"
+    "revision": "e41ebfe092fa3d5bbaf5",
+    "url": "/chess/static/css/main.09eb3a45.chunk.css"
   },
   {
     "revision": "1cc92a302ad3f92840ba",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/chess/static/js/2.56c8c62e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "777c5e9feb0796b4e054",
-    "url": "/chess/static/js/main.44a864ec.chunk.js"
+    "revision": "e41ebfe092fa3d5bbaf5",
+    "url": "/chess/static/js/main.bebea4d8.chunk.js"
   },
   {
     "revision": "fe54e1bce9cc9d1a3729",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/chess/precache-manifest.aa3626011c919109545e044b6b11082a.js"
+  "/chess/precache-manifest.f9b7e9c93bee22028d46e7215335be52.js"
 );
 
 self.addEventListener('message', (event) => {
