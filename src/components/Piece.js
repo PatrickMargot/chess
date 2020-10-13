94b1348d58f71061155056b94d7d621ec8d25e91
Patrick <margotpatrick02@gmail.com>
2020-10-13 14:16:45 +0200

    lightened stroke color

--- src/components/Piece.js
+++ src/components/Piece.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, rgbToHex } from '@material-ui/core/styles';
 import { motion, AnimatePresence, useDragControls } from 'framer-motion';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
@@ -27,7 +27,7 @@ const useStyles = makeStyles(theme => ({
   icon: {
     width: '70% !important',
     height: '70%',
-    stroke: 'black',
+    stroke: '#444',
     strokeWidth: 15,
   },
 }));
